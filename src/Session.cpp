a108278265835759edca06f52185d4f77cb59269
Václav Kubernát <kubernat@cesnet.cz>
2021-09-06 10:04:03 +0200

    Add Session::setItem and Session::getData
    There was a discussion about what about type of string arguments to use.
    The strings must be null-terminated. There are three options:
    1) const char*
        + doesn't copy the string
        + somewhat safe - no one creates C-strings manually in C++
        - most C way to do it
        - must always use .c_str() if you have a std::string
    2) std::string_view
        + has implicit conversions from std::string and const char* - easy
          to use as an argument
        + doesn't copy the string
        + most modern C++ way to do it
        - less safe than C-strings - one might construct it from two
          std::string iterators and then it's not null-terminated
    3) std::string
        + implicit conversion from const char* - doesn't need .c_str()
        + very safe - std::string is guaranteed to be null-terminated
        + it's more C++ than C-strings
        - always copies the string
        - no implicit conversion from std::string_view (must use ctor)
    
    In the end, I chose number 1, because of simplicity and relative safety.
    Users will have to deal with using .c_str() (if they have std::string).
    
    Change-Id: I6ecdc584644f16e5f6d1cd4931686b3cb52abebc

--- src/Session.cpp
+++ src/Session.cpp
@@ -12,6 +12,7 @@ extern "C" {
 #include <sysrepo-cpp/Session.hpp>
 #include "utils/exception.hpp"
 
+using namespace std::string_literals;
 namespace sysrepo {
 
 Session::Session(sr_session_ctx_s* sess, std::shared_ptr<sr_conn_ctx_s> conn)
@@ -23,5 +24,57 @@ Session::Session(sr_session_ctx_s* sess, std::shared_ptr<sr_conn_ctx_s> conn)
 Datastore Session::activeDatastore() const
 {
     return static_cast<Datastore>(sr_session_get_ds(m_sess.get()));
+}
+
+/**
+ * Set a value of leaf, leaf-list, or create a list or a presence container. The changes are applied only after calling
+ * Session::applyChanges.
+ *
+ * Wraps `sr_set_item_str`.
+ *
+ * @param path Path of the element to be changed.
+ * @param value Value of the element to be changed. Can be `nullptr`.
+ */
+void Session::setItem(const char* path, const char* value)
+{
+    auto res = sr_set_item_str(m_sess.get(), path, value, nullptr, 0);
+
+    throwIfError(res, "Session::setItem: Couldn't set '"s + path + "' to '" + "'" + value + "'");
+}
+
+/**
+ * Retrieves a tree specified by the provided XPath.
+ *
+ * Wraps `sr_get_data`.
+ *
+ * @param path Path of the element to be retrieved.
+ *
+ * @returns std::nullopt if no matching data found, otherwise the requested data.
+ */
+std::optional<libyang::DataNode> Session::getData(const char* path)
+{
+    lyd_node* node;
+    auto res = sr_get_data(m_sess.get(), path, 0, 0, 0, &node);
+
+    throwIfError(res, "Session::getData: Couldn't get '"s + path + "'");
+
+    if (!node) {
+        return std::nullopt;
+    }
+
+    return libyang::wrapRawNode(node);
+}
+
+/**
+ * Applies changes made in this Session.
+ *
+ * Wraps `sr_apply_changes`.
+ * @param timeout Optional timeout for change callbacks.
+ */
+void Session::applyChanges(std::chrono::milliseconds timeout)
+{
+    auto res = sr_apply_changes(m_sess.get(), timeout.count());
+
+    throwIfError(res, "Session::applyChanges: Couldn't apply changes");
 }
 }
