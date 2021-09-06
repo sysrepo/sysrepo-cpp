88c3d17035b503567abeb8a2a34beecabb242135
Václav Kubernát <kubernat@cesnet.cz>
2021-09-06 10:04:03 +0200

    Add Session::setItem and Session::getData
    There was a discussion about what about type of string arguments to use.
    The strings must be null-terminated. There are three options:
    1) const char*
        + doesn't copy the string
        + somewhat safe - no one creates C-strings manually in C++
        - most C way to do it
        - must always use .c_str() if you have a std::string
    2) std::string_view
        + has implicit conversions from std::string and const char* - easy
          to use as an argument
        + doesn't copy the string
        + most modern C++ way to do it
        - less safe than C-strings - one might construct it from two
          std::string iterators and then it's not null-terminated
    3) std::string
        + implicit conversion from const char* - doesn't need .c_str()
        + very safe - std::string is guaranteed to be null-terminated
        + it's more C++ than C-strings
        - always copies the string
        - no implicit conversion from std::string_view (must use ctor)
    
    In the end, I chose number 1, because of simplicity and relative safety.
    Users will have to deal with using .c_str() (if they have std::string).
    
    Change-Id: I6ecdc584644f16e5f6d1cd4931686b3cb52abebc
    Depends-on: https://gerrit.cesnet.cz/c/CzechLight/libyang-cpp/+/4632

--- include/sysrepo-cpp/Session.hpp
+++ include/sysrepo-cpp/Session.hpp
@@ -7,11 +7,15 @@
 */
 #pragma once
 
+#include <chrono>
 #include <memory>
+#include <optional>
+#include <libyang-cpp/DataNode.hpp>
 #include <sysrepo-cpp/Enum.hpp>
 
 struct sr_conn_ctx_s;
 struct sr_session_ctx_s;
+struct sr_val_s;
 
 namespace sysrepo {
 class Connection;
@@ -19,6 +23,11 @@ class Connection;
 class Session {
 public:
     Datastore activeDatastore() const;
+    // TODO: allow all arguments
+    void setItem(const char* path, const char* value);
+    // TODO: allow all arguments
+    std::optional<libyang::DataNode> getData(const char* path);
+    void applyChanges(std::chrono::milliseconds timeout = std::chrono::milliseconds{0});
 private:
     friend Connection;
     Session(sr_session_ctx_s* sess, std::shared_ptr<sr_conn_ctx_s> conn);
