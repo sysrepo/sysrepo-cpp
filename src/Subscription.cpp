a10ca19f58c0fb291416bb6d887cb216d09370af
Václav Kubernát <kubernat@cesnet.cz>
2021-09-09 08:35:33 +0200

    Add support for module change subscriptions
    I'm using a list instead of a vector to save the callbacks, because the
    addresses would change when the vector reallocates.
    
    Note: If ever std::list gives problems I can just allocate the callbacks
    on the heap. Or allocate the container on the heap.
    
    Change-Id: I993ed85265b41f2f04cb2f92a97bcc9c7b8815c8

---
+++ src/Subscription.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Václav Kubernát <kubernat@cesnet.cz>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#include <sysrepo-cpp/Subscription.hpp>
+extern "C" {
+#include <sysrepo.h>
+}
+#include "utils/enum.hpp"
+#include "utils/exception.hpp"
+#include "utils/utils.hpp"
+
+namespace sysrepo {
+Subscription::Subscription(std::shared_ptr<sr_session_ctx_s> sess)
+    : m_sess(sess)
+{
+}
+
+void Subscription::saveContext(sr_subscription_ctx_s* ctx)
+{
+    if (!m_sub) {
+        m_sub = std::shared_ptr<sr_subscription_ctx_s>(ctx, sr_unsubscribe);
+    }
+}
+
+namespace {
+int moduleChangeCb(sr_session_ctx_t* session, uint32_t subscriptionId, const char* moduleName, const char* subXPath, sr_event_t event, uint32_t requestId, void* privateData)
+{
+    auto cb = reinterpret_cast<ModuleChangeCb*>(privateData);
+    return static_cast<int>((*cb)(wrapUnmanagedSession(session), subscriptionId, moduleName, subXPath ? std::optional<std::string_view>{subXPath} : std::nullopt, toEvent(event), requestId));
+}
+}
+
+void Subscription::onModuleChange(const char* moduleName, ModuleChangeCb cb, const char* xpath, uint32_t priority, const SubOptions opts)
+{
+    auto& cbRef = m_moduleChangeCbs.emplace_back(cb);
+    sr_subscription_ctx_s* ctx = m_sub.get();
+
+    auto res = sr_module_change_subscribe(m_sess.get(), moduleName, xpath, moduleChangeCb, reinterpret_cast<void*>(&cbRef), priority, toSubOptions(opts), &ctx);
+    throwIfError(res, "Couldn't create module change subscription");
+
+    saveContext(ctx);
+}
+
+Subscription::Subscription(Subscription&& other) noexcept
+    : m_sub(other.m_sub)
+    , m_sess(other.m_sess)
+    , m_moduleChangeCbs(std::move(other.m_moduleChangeCbs))
+{
+}
+
+Subscription& Subscription::operator=(Subscription&& other) noexcept
+{
+    if (this == &other) {
+        return *this;
+    }
+
+    m_sub = other.m_sub;
+    m_sess = other.m_sess;
+    m_moduleChangeCbs = std::move(other.m_moduleChangeCbs);
+
+    return *this;
+}
+}
