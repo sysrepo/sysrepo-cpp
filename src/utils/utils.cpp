06fb4d24cf8431f89eebf330c825a6e2293a07f5
Václav Kubernát <kubernat@cesnet.cz>
2021-09-09 08:35:33 +0200

    Add support for module change subscriptions
    I'm using a list instead of a vector to save the callbacks, because the
    addresses would change when the vector reallocates.
    
    Note: If ever std::list gives problems I can just allocate the callbacks
    on the heap. Or allocate the container on the heap.
    
    Change-Id: I993ed85265b41f2f04cb2f92a97bcc9c7b8815c8

---
+++ src/utils/utils.cpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Václav Kubernát <kubernat@cesnet.cz>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+*/
+#include "utils.hpp"
+
+namespace sysrepo {
+Session wrapUnmanagedSession(sr_session_ctx_s* session);
+}
---
+++ src/utils/utils.hpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Václav Kubernát <kubernat@cesnet.cz>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+*/
+#pragma once
+#include <sysrepo-cpp/Session.hpp>
+struct sr_session_ctx_s;
+
+namespace sysrepo {
+Session wrapUnmanagedSession(sr_session_ctx_s* session)
+{
+    return Session{session};
+}
+}
