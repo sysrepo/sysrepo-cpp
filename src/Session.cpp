1fb5652a74dcbdbd065b9a03928812243aa83e6b
Václav Kubernát <kubernat@cesnet.cz>
2021-11-02 15:16:15 +0100

    Add Session::moveItem
    Change-Id: Idd0b27295e22b0c2d2afa598a48db9743f3537a4

--- src/Session.cpp
+++ src/Session.cpp
@@ -87,6 +87,24 @@ void Session::deleteItem(const char* path, const EditOptions opts)
 }
 
 /**
+ * Moves item (a list or a leaf-list) specified by `path`.
+ * @param path Node to move.
+ * @param move Specifies the type of the move.
+ * @param keys_or_value list instance specified on the format [key1="val1"][key2="val2"] or a leaf-list value. Can be
+ * nullptr for the `First` `Last` move types.
+ * @param origin Origin of the value.
+ * @param opts Options modifying the behavior of this method.
+ */
+void Session::moveItem(const char* path, const MovePosition move, const char* keys_or_value, const char* origin, const EditOptions opts)
+{
+    // sr_move_item has separate arguments for list keys and leaf-list values, but the C++ api has just one. It is OK if
+    // both of the arguments are the same. https://github.com/sysrepo/sysrepo/issues/2621
+    auto res = sr_move_item(m_sess.get(), path, toMovePosition(move), keys_or_value, keys_or_value, origin, toEditOptions(opts));
+
+    throwIfError(res, "Session::moveItem: Can't move '"s + path + "'");
+}
+
+/**
  * Retrieves a tree specified by the provided XPath.
  *
  * Wraps `sr_get_data`.
