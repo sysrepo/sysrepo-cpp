ff15f900ec40fb985482133c2982cfd76e675d99
Václav Kubernát <kubernat@cesnet.cz>
2021-11-09 13:50:47 +0100

    Make a bunch of methods const
    Change-Id: I3732a06facbabf8509bd00e9ed45a67f074f0453

--- include/sysrepo-cpp/Session.hpp
+++ include/sysrepo-cpp/Session.hpp
@@ -32,22 +32,22 @@ public:
     Datastore activeDatastore() const;
     void switchDatastore(const Datastore ds) const;
     // TODO: allow all arguments
-    void setItem(const char* path, const char* value);
-    void editBatch(libyang::DataNode edit, const DefaultOperation op);
-    void deleteItem(const char* path, const EditOptions opts = sysrepo::EditOptions::Default);
-    void moveItem(const char* path, const MovePosition move, const char* keys_or_value, const char* origin = nullptr, const EditOptions opts = sysrepo::EditOptions::Default);
+    void setItem(const char* path, const char* value) const;
+    void editBatch(libyang::DataNode edit, const DefaultOperation op) const;
+    void deleteItem(const char* path, const EditOptions opts = sysrepo::EditOptions::Default) const;
+    void moveItem(const char* path, const MovePosition move, const char* keys_or_value, const char* origin = nullptr, const EditOptions opts = sysrepo::EditOptions::Default) const;
     // TODO: allow all arguments
     std::optional<libyang::DataNode> getData(const char* path) const;
-    void applyChanges(std::chrono::milliseconds timeout = std::chrono::milliseconds{0});
-    void discardChanges();
-    void copyConfig(const Datastore source, const char* moduleName = nullptr, std::chrono::milliseconds timeout = std::chrono::milliseconds{0});
-    libyang::DataNode sendRPC(libyang::DataNode input, std::chrono::milliseconds timeout = std::chrono::milliseconds{0});
+    void applyChanges(std::chrono::milliseconds timeout = std::chrono::milliseconds{0}) const;
+    void discardChanges() const;
+    void copyConfig(const Datastore source, const char* moduleName = nullptr, std::chrono::milliseconds timeout = std::chrono::milliseconds{0}) const;
+    libyang::DataNode sendRPC(libyang::DataNode input, std::chrono::milliseconds timeout = std::chrono::milliseconds{0}) const;
 
-    [[nodiscard]] Subscription onModuleChange(const char* moduleName, ModuleChangeCb cb, const char* xpath = nullptr, uint32_t priority = 0, const SubscribeOptions opts = SubscribeOptions::Default);
-    [[nodiscard]] Subscription onOperGetItems(const char* moduleName, OperGetItemsCb cb, const char* xpath = nullptr, const SubscribeOptions opts = SubscribeOptions::Default);
-    [[nodiscard]] Subscription onRPCAction(const char* xpath, RpcActionCb cb, uint32_t priority = 0, const SubscribeOptions opts = SubscribeOptions::Default);
+    [[nodiscard]] Subscription onModuleChange(const char* moduleName, ModuleChangeCb cb, const char* xpath = nullptr, uint32_t priority = 0, const SubscribeOptions opts = SubscribeOptions::Default) const;
+    [[nodiscard]] Subscription onOperGetItems(const char* moduleName, OperGetItemsCb cb, const char* xpath = nullptr, const SubscribeOptions opts = SubscribeOptions::Default) const;
+    [[nodiscard]] Subscription onRPCAction(const char* xpath, RpcActionCb cb, uint32_t priority = 0, const SubscribeOptions opts = SubscribeOptions::Default) const;
 
-    ChangeCollection getChanges(const char* xpath = "//.");
+    ChangeCollection getChanges(const char* xpath = "//.") const;
 
     const libyang::Context getContext() const;
 
