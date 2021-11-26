c0d9e9e1dea340cb9cb289a274fde49d70ea17ac
Václav Kubernát <kubernat@cesnet.cz>
2021-11-26 14:45:27 +0100

    Add streaming operator for NotificationType
    Change-Id: I7a4e74075fd4a5e71374cbdb506c24fa86645bf2

---
+++ src/Enum.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Václav Kubernát <kubernat@cesnet.cz>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+*/
+#include <ostream>
+#include <sysrepo-cpp/Enum.hpp>
+
+namespace sysrepo {
+std::ostream& operator<<(std::ostream& os, const NotificationType& type)
+{
+    switch (type) {
+    case sysrepo::NotificationType::Realtime:
+        return os << "sysrepo::NotificationType::Realtime";
+    case sysrepo::NotificationType::Replay:
+        return os << "sysrepo::NotificationType::Replay";
+    case sysrepo::NotificationType::ReplayComplete:
+        return os << "sysrepo::NotificationType::ReplayComplete";
+    case sysrepo::NotificationType::Terminated:
+        return os << "sysrepo::NotificationType::Terminated";
+    case sysrepo::NotificationType::Modified:
+        return os << "sysrepo::NotificationType::Modified";
+    case sysrepo::NotificationType::Suspended:
+        return os << "sysrepo::NotificationType::Suspended";
+    case sysrepo::NotificationType::Resumed:
+        return os << "sysrepo::NotificationType::Resumed";
+    default:
+        return os << "[unknown event type]";
+    }
+}
+}
