8de6fb53ce9af38094d7b27f5f91b30136f3575b
Jan Kundrát <jan.kundrat@cesnet.cz>
2023-02-23 14:35:25 +0100

    wrap sr_get_node() for retrieving a single value
    Based on popular request, sysrepo got a function for retrieving a single
    libyang data node; sometimes it's more efficient compared to
    `sr_get_data`.
    
    Bug: https://github.com/sysrepo/sysrepo-cpp/issues/13
    Change-Id: I9a797c942c638c6a0f7b03c07a1456f8e9b4aba7

--- src/Session.cpp
+++ src/Session.cpp
@@ -189,6 +189,28 @@ std::optional<libyang::DataNode> Session::getData(const std::string& path) const
 }
 
 /**
+ * @brief Returns a single value matching the provided XPath.
+ *
+ * Wraps `sr_get_node`.
+ *
+ * @param path XPath which corresponds to the data that should be retrieved.
+ * @returns std::nullopt if no matching node found, otherwise the requested data node.
+ */
+std::optional<libyang::DataNode> Session::getOneNode(const std::string& path) const
+{
+    sr_data_t* data;
+    auto res = sr_get_node(m_sess.get(), path.c_str(), 0, &data);
+
+    throwIfError(res, "Session::getOneNode: Couldn't get '"s + path + "'");
+
+    if (!data) {
+        return std::nullopt;
+    }
+
+    return wrapSrData(m_sess, data);
+}
+
+/**
  * @brief Retrieves changes that have not been applied yet.
  *
  * Do NOT change the returned data. It is possible to duplicate them. After the changes get applied or discarded, they
