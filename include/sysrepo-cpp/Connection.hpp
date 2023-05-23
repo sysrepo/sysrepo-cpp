sysrepo/sysrepo-cpp

commit ff7e8c10c456e69569a179590e805998ee294cf1

Wrap sr_discard_items(), the sr_discard_oper_changes() is deprecated
This is an ABI breaking change.

Signed-off-by: Petr Gotthard <petr.gotthard@centrum.cz>
Change-Id: Id02dff6920e019ba0a35f9ee4d6f064211a7d438

src/Connection.cpp
@@ -56,6 +56,7 @@ Session Connection::sessionStart(sysrepo::Datastore datastore)
     return Session{sess, ctx};
 }
 
+[[deprecated]]
 void Connection::discardOperationalChanges(const std::optional<std::string>& xpath, std::optional<Session> session, std::chrono::milliseconds timeout)
 {
     auto res = sr_discard_oper_changes(ctx.get(), session ? session->m_sess.get() : nullptr, xpath ? xpath->c_str() : nullptr, timeout.count());

CMakeLists.txt
@@ -28,7 +28,7 @@ option(WITH_EXAMPLES "Build examples" ON)
 
 find_package(PkgConfig)
 pkg_check_modules(LIBYANG_CPP REQUIRED libyang-cpp=1.1.0 IMPORTED_TARGET)
-pkg_check_modules(SYSREPO REQUIRED sysrepo>=2.2.14 IMPORTED_TARGET)
+pkg_check_modules(SYSREPO REQUIRED sysrepo>=2.2.71 IMPORTED_TARGET)
 set(SYSREPO_CPP_PKG_VERSION "1.2.0")
 
 include_directories(${CMAKE_CURRENT_SOURCE_DIR}/include)

include/sysrepo-cpp/Session.hpp
@@ -82,6 +82,7 @@ public:
     void setItem(const std::string& path, const std::optional<std::string>& value, const EditOptions opts = sysrepo::EditOptions::Default);
     void editBatch(libyang::DataNode edit, const DefaultOperation op);
     void deleteItem(const std::string& path, const EditOptions opts = sysrepo::EditOptions::Default);
+    void discardItems(const std::optional<std::string>& xpath);
     void moveItem(const std::string& path, const MovePosition move, const std::optional<std::string>& keys_or_value, const std::optional<std::string>& origin = std::nullopt, const EditOptions opts = sysrepo::EditOptions::Default);
     // TODO: allow all arguments
     std::optional<libyang::DataNode> getData(const std::string& path) const;

src/Session.cpp
@@ -119,6 +119,21 @@ void Session::deleteItem(const std::string& path, const EditOptions opts)
 }
 
 /**
+ * Prepare to discard nodes matching the specified xpath (or all if not set) previously set by the session connection.
+ * Usable only for sysrepo::Datastore::Operational. The changes are applied only after calling Session::applyChanges.
+ *
+ * Wraps `sr_discard_items`.
+ *
+ * @param xpath Expression filtering the nodes to discard, nullopt for all nodes.
+ */
+void Session::discardItems(const std::optional<std::string>& xpath)
+{
+    auto res = sr_discard_items(m_sess.get(), xpath ? xpath->c_str() : nullptr);
+
+    throwIfError(res, "Session::discardItems: Can't discard "s + (xpath ? "'"s + *xpath + "'" : "all nodes"s));
+}
+
+/**
  * Moves item (a list or a leaf-list) specified by `path`.
  * @param path Node to move.
  * @param move Specifies the type of the move.

tests/session.cpp
@@ -116,8 +116,9 @@ TEST_CASE("session")
         sess.applyChanges();
         REQUIRE(!sess.getData("/test_module:leafInt32"));
 
-        // Using discardOperationalChanges makes the leaf visible again (in the operational datastore).
-        conn->discardOperationalChanges("/test_module:leafInt32");
+        // Using discardItems makes the leaf visible again (in the operational datastore).
+        sess.discardItems("/test_module:leafInt32");
+        sess.applyChanges();
         REQUIRE(sess.getData("/test_module:leafInt32")->asTerm().valueStr() == "123");
     }
 

commit 1e0b083aee6862764514c2dc21bfd1da0e087687

[[deprecated]] belong to the header
Fixes: ff7e8c1 (Wrap sr_discard_items(), the sr_discard_oper_changes() is deprecated)
Change-Id: Idffb66cca147639a033e2d3946e76a2d8b0741aa

include/sysrepo-cpp/Connection.hpp
@@ -28,7 +28,7 @@ public:
     Connection();
     Session sessionStart(sysrepo::Datastore datastore = sysrepo::Datastore::Running);
 
-    void discardOperationalChanges(const std::optional<std::string>& xpath = std::nullopt, std::optional<Session> session = std::nullopt, std::chrono::milliseconds timeout = std::chrono::milliseconds{0});
+    [[deprecated("Use sysrepo::Session::discardItems")]] void discardOperationalChanges(const std::optional<std::string>& xpath = std::nullopt, std::optional<Session> session = std::nullopt, std::chrono::milliseconds timeout = std::chrono::milliseconds{0});
 
     friend Connection wrapUnmanagedConnection(std::shared_ptr<sr_conn_ctx_s> conn);
     friend Session;

src/Connection.cpp
@@ -56,7 +56,6 @@ Session Connection::sessionStart(sysrepo::Datastore datastore)
     return Session{sess, ctx};
 }
 
-[[deprecated]]
 void Connection::discardOperationalChanges(const std::optional<std::string>& xpath, std::optional<Session> session, std::chrono::milliseconds timeout)
 {
     auto res = sr_discard_oper_changes(ctx.get(), session ? session->m_sess.get() : nullptr, xpath ? xpath->c_str() : nullptr, timeout.count());