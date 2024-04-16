52211bd8da15f48e0fd23bbe51bccfd59d1785da
Tomáš Pecka <tomas.pecka@cesnet.cz>
2024-04-16 12:13:19 +0200

    wrap max_depth argument in sr_get_data
    sr_get_data function can also return elements up to some maximum depth.
    Let's add this parameter to our Session::getData method.
    
    This will be useful in the RESTCONF server we implement.
    
    Change-Id: I1b2262af07bdb0165231035039db35ec1497d743

--- src/Session.cpp
+++ src/Session.cpp
@@ -187,13 +187,14 @@ libyang::DataNode wrapSrData(std::shared_ptr<sr_session_ctx_s> sess, sr_data_t* 
  * Wraps `sr_get_data`.
  *
  * @param path XPath which corresponds to the data that should be retrieved.
+ * @param maxDepth Maximum depth of the selected subtrees. 0 is unlimited, 1 will not return any descendant nodes.
  * @param opts GetOptions overriding default behaviour
  * @returns std::nullopt if no matching data found, otherwise the requested data.
  */
-std::optional<libyang::DataNode> Session::getData(const std::string& path, const GetOptions opts) const
+std::optional<libyang::DataNode> Session::getData(const std::string& path, int maxDepth, const GetOptions opts) const
 {
     sr_data_t* data;
-    auto res = sr_get_data(m_sess.get(), path.c_str(), 0, 0, toGetOptions(opts), &data);
+    auto res = sr_get_data(m_sess.get(), path.c_str(), maxDepth, 0, toGetOptions(opts), &data);
 
     throwIfError(res, "Session::getData: Couldn't get '"s + path + "'", m_sess.get());
 
