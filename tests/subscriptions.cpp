7fb68dd9c3fd8b8f05b77721d065caf694721936
Jan Kundrát <jan.kundrat@cesnet.cz>
2025-05-28 18:26:20 +0200

    refactor: split between "Callbacks" and "Changes"
    Callbacks are for definition of return types of various onXYZ()
    functions (aka, the subscribing functions), while Changes are stuff
    which are useful within the bodies of user-provided callback
    implementations.
    
    Change-Id: I43e125ebc0897a67f50d5272f10aecfaca1eb471

--- tests/subscriptions.cpp
+++ tests/subscriptions.cpp
@@ -9,6 +9,7 @@
 #include <atomic>
 #include <doctest/doctest.h>
 #include <pretty_printers.hpp>
+#include <sysrepo-cpp/Changes.hpp>
 #include <sysrepo-cpp/Subscription.hpp>
 #include <sysrepo-cpp/utils/utils.hpp>
 #include <sysrepo-cpp/utils/exception.hpp>
