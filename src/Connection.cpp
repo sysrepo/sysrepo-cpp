5672ee94e0c8070ba22af84e49da295326ffbb09
Humblesaw <kinglonghead@gmail.com>
2025-12-16 19:50:15 +0100

    API add: new API functions
    Wrappers for sr_install_modules, sr_remove_modules.
    
    Change-Id: Iebfebc290f8494b860ef49c00c293cbbce90ff7f

--- src/Connection.cpp
+++ src/Connection.cpp
@@ -6,6 +6,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
 */
 
+#include <algorithm>
 extern "C" {
 #include <sysrepo.h>
 }
@@ -16,6 +17,81 @@ extern "C" {
 #include "utils/misc.hpp"
 
 namespace sysrepo {
+
+namespace {
+/**
+ * @brief Convert a list of strings to a null-terminated vector of C strings, this
+ * might be dangerous if invoked on a temporary @p vec.
+ *
+ * @param[in] vec Vector of strings.
+ * @return Converted null-terminated vector of C strings.
+ */
+std::vector<const char*> unsafeCStringArray(const std::vector<std::string>& vec)
+{
+    std::vector<const char*> res;
+    res.reserve(vec.size() + 1 /* trailing nullptr */);
+    std::transform(vec.begin(), vec.end(), std::back_inserter(res), [](const auto& x) { return x.c_str(); });
+    res.push_back(nullptr);
+    return res;
+}
+}
+
+/**
+ * @brief Install all YANG modules specified in the @p schemaPaths list.
+ *
+ * Wraps `sr_install_modules2`
+ */
+void Connection::installModules(const std::vector<struct ModuleInstallation>& modules,
+                                const std::optional<std::filesystem::path>& searchDirs,
+                                const std::variant<std::monostate, std::filesystem::path, std::string>& initialData,
+                                const libyang::DataFormat dataFormat)
+{
+    std::vector<const char*> features;
+    std::vector<std::vector<const char*>> allFeatures;
+    std::string schema;
+    std::vector<std::string> allSchemas;
+    std::vector<sr_install_mod_t> mods;
+    const std::filesystem::path* initPath = std::get_if<std::filesystem::path>(&initialData);
+    const std::string* initData = std::get_if<std::string>(&initialData);
+
+    /* prepare memory so that passed pointers to sr_install_modules2() are valid */
+    for (auto& module : modules) {
+        /* save so that .data() call is valid */
+        allFeatures.push_back(unsafeCStringArray(module.features));
+
+        if (holds_alternative<std::filesystem::path>(module.schema)) {
+            /* save so that .c_str() call is valid */
+            allSchemas.push_back(std::get<std::filesystem::path>(module.schema).string());
+        } else {
+            allSchemas.push_back(std::get<std::string>(module.schema));
+        }
+    }
+    for (std::size_t i = 0; i < modules.size(); ++i) {
+        mods.push_back(sr_install_mod_t{.schema_path = allSchemas[i].c_str(),
+                                        .features = allFeatures[i].data(),
+                                        .module_ds = {nullptr},
+                                        .owner = modules[i].owner ? modules[i].owner->c_str() : nullptr,
+                                        .group = modules[i].group ? modules[i].group->c_str() : nullptr,
+                                        .perm = modules[i].permissions});
+    }
+    auto res = sr_install_modules2(ctx.get(), mods.data(), mods.size(), searchDirs ? searchDirs->c_str() : nullptr,
+            initData ? initData->c_str() : nullptr, initPath ? initPath->c_str() : nullptr, static_cast<LYD_FORMAT>(dataFormat));
+
+    throwIfError(res, "Couldn't install modules");
+}
+
+/**
+ * @brief Remove all YANG modules specified in the @p modules list.
+ *
+ * Wraps `sr_remove_modules`
+ */
+void Connection::removeModules(const std::vector<std::string>& modules)
+{
+    auto modulesC = unsafeCStringArray(modules);
+    auto res = sr_remove_modules(ctx.get(), modulesC.data(), 1);
+
+    throwIfError(res, "Couldn't remove modules");
+}
 
 /**
  * Creates a new connection to sysrepo. The lifetime of it is managed automatically.
