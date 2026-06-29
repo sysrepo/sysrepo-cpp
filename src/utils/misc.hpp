44c0696ce5776bf240001c7b212041e08df6f373
Tomáš Pecka <tomas.pecka@cesnet.cz>
2026-06-29 14:33:13 +0200

    refactor: extract constructXPathFilter into shared utils
    Move it into utils so it can be reused by the upcoming dynamic
    subscription modify methods.
    
    Change-Id: Ibbd1905dc08994bb6aa961f89722bffba5392edb

--- src/utils/misc.hpp
+++ src/utils/misc.hpp
@@ -13,4 +13,5 @@ namespace sysrepo {
 std::timespec toTimespec(std::chrono::time_point<std::chrono::system_clock, std::chrono::nanoseconds>);
 std::chrono::time_point<std::chrono::system_clock, std::chrono::nanoseconds> toTimePoint(std::timespec ts);
 void checkNoThreadFlag(const SubscribeOptions opts, const std::optional<FDHandling>& callbacks);
+std::optional<std::string> constructXPathFilter(const std::optional<SubscribedNotificationsFilter>& filter);
 }
